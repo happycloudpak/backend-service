cf780349455870d2ee8064a76121dee6ce9322ff
hiondal <hiondal@gmail.com>
2020-11-27 13:55:39 +0900

    aaa

--- src/main/java/com/svcmesh/SwaggerConfig.java
+++ src/main/java/com/svcmesh/SwaggerConfig.java
@@ -23,6 +23,6 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {		
-		return new ApiInfoBuilder().title("REST API").description("REST aPI Test").version("1.0.0").build();
+		return new ApiInfoBuilder().title("REST API").description("REST aPI Test").version("1.0.1").build();
 	}
 }
